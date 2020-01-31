012a09595a351b432090aace2457c80ce827a5dc
mikayla_mendez@ymail.com <mikayla_mendez@ymail.com>
2020-01-31 17:02:18 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReadUserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
